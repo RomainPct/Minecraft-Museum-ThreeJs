d6b2a50befe974e89ee238d61198819d7229518b
RomainPct <romain.penchenat@gmail.com>
2020-03-24 13:43:43 +0100

    Add sides to the path

--- src/script/objects/Floor.js
+++ src/script/objects/Floor.js
@@ -5,6 +5,7 @@ export default class Floor {
 
     constructor(_scene, _textureLoader, _cubesNumber) {
         const depth = (_cubesNumber * 5) + 20
+
         this.floor = new THREE.Mesh(
             new THREE.PlaneBufferGeometry(120, depth),
             this.getFloorMaterial(depth, _textureLoader)
@@ -12,6 +13,17 @@ export default class Floor {
         this.floor.rotation.x = Math.PI * -0.5
         this.floor.position.z = (depth * -0.5) + 20
         _scene.add(this.floor)
+
+        const leftSide = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(0.99, 0.5, depth),
+            this.getSideMaterial(depth, _textureLoader)
+        )
+        leftSide.position.z = (depth * -0.5) + 20
+        leftSide.position.x = -4
+        _scene.add(leftSide)
+        const rightSide = leftSide.clone()
+        rightSide.position.x = 4
+        _scene.add(rightSide)
     }
 
     getFloorMaterial(_depth, _textureLoader) {
@@ -23,6 +35,17 @@ export default class Floor {
         texture.magFilter = THREE.NearestFilter
         texture.minFilter = THREE.NearestFilte
         return new THREE.MeshStandardMaterial({ map: texture })
+    }
+
+    getSideMaterial(_depth, _textureLoader) {
+        // const texture = _textureLoader.load(floorColorSource)
+        // texture.repeat.x = 120
+        // texture.repeat.y = _depth
+        // texture.wrapS = THREE.RepeatWrapping
+        // texture.wrapT = THREE.RepeatWrapping
+        // texture.magFilter = THREE.NearestFilter
+        // texture.minFilter = THREE.NearestFilte
+        return new THREE.MeshStandardMaterial({ color: 0xffffff })
     }
 
 }
